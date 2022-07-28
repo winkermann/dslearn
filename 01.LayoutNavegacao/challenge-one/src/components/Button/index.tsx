ce3f74feff0ce6e02885e30d28e0fca08a5651f3
Winkermann M <willmeschke@gmail.com>
2022-07-28 13:45:41 -0300

    Button icon and home page css refactor

---
+++ 01.LayoutNavegacao/challenge-one/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Button = () => {
+  return (
+    <div className="button-container base-card-container">
+      <p>VER CATÁLOGO</p>
+    </div>
+  );
+};
+
+export default Button;
--- 01.LayoutNavegacao/challenge-one/src/pages/Home/index.tsx
+++ 01.LayoutNavegacao/challenge-one/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { ReactComponent as MainImage } from '../../assets/img/car-header.svg';
 import Navbar from '../../components/Navbar';
+import Button from '../../components/Button';
+
 import './styles.css';
 
 const Home = () => {
@@ -9,7 +11,7 @@ const Home = () => {
       <div className="home-container">
         <div className="base-card-container">
           <div className="home-content-container">
-            <div>
+            <div className="home-content-text">
               <h1>O carro perfeito para você</h1>
               <p>
                 Conheça nossos carros e dê mais um passo na realização do seu
@@ -22,11 +24,13 @@ const Home = () => {
           </div>
         </div>
         <div className="section-card-container">
-          <div>
-            <button>Ver Catálogo</button>
-          </div>
-          <div>
-            <p>Comece agora a navegar</p>
+          <div className="subsection-container">
+            <div>
+              <Button />
+            </div>
+            <div>
+              <span>Comece agora a navegar</span>
+            </div>
           </div>
         </div>
       </div>
